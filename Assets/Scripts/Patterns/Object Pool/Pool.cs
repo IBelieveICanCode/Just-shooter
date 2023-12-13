4f5ea9a6adaedac933d931ae0973a3974c8e041c
Serj <Zervanseryi@gmail.com>
2023-12-13 20:20:00 +0200

    Added system for future gun system
    Factory + objectPool. + Singleton

---
+++ Assets/Scripts/Patterns/Object Pool/Pool.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ObjectPool
+{
+	public class Pool<T> : IEnumerable where T : IResettable
+	{
+		private List<T> _members = new List<T>();
+		private HashSet<T> _unavailable = new HashSet<T>();
+		private IFactory<T> _factory;
+
+		private const int DefaultPoolSize = 5;
+
+		public Pool(IFactory<T> factory) : this(factory, DefaultPoolSize) { }
+
+		public Pool(IFactory<T> factory, int poolSize)
+		{
+			this._factory = factory;
+
+			for (int i = 0; i < poolSize; i++)
+			{
+				Create();
+			}
+		}
+
+		public T Allocate()
+		{
+			for (int i = 0; i < _members.Count; i++)
+			{
+				if (!_unavailable.Contains(_members[i]))
+				{
+					_unavailable.Add(_members[i]);
+					return _members[i];
+				}
+			}
+			T newMember = Create();
+			_unavailable.Add(newMember);
+			return newMember;
+		}
+
+		public void Release(T member)
+		{
+			member.Reset();
+			_unavailable.Remove(member);
+		}
+
+		T Create()
+		{
+			T member = _factory.Create();
+			_members.Add(member);
+			return member;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return _members.GetEnumerator();
+		}
+	}
+}
---
+++ Assets/Scripts/Patterns/Object Pool/PrefabFactory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ObjectPool
+{
+    public class PrefabFactory<T> : IFactory<T> where T : MonoBehaviour
+    {
+        private GameObject _prefab;
+        private string _name;
+        private int  _index = 0;
+
+        public PrefabFactory(GameObject prefab) : this(prefab, prefab.name) { }
+
+        public PrefabFactory(GameObject prefab, string name)
+        {
+            this._prefab = prefab;
+            this._name = name;
+        }
+
+        public T Create()
+        {
+            GameObject tempGameObject = GameObject.Instantiate(_prefab) as GameObject;
+            tempGameObject.name = _name + _index.ToString();
+            //T objectOfType = tempGameObject.TryGetComponent<T>();
+            T objectOfType;
+
+            if (!tempGameObject.TryGetComponent<T>(out objectOfType))
+            {
+                objectOfType = tempGameObject.AddComponent<T>();
+            }
+
+            _index++;
+            return objectOfType;
+        }
+    }
+}
